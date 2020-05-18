24b52bd828ded2868d08bfa1873806b4b07dda97
kororo11 <kororo11@naver.com>
2020-05-18 23:21:50 +0900

    Home Controller: One
    임시로 가짜 데이터 db.js 을 만들었다.
    db.js 에서 videos 를 export 해서 controller 에서 두번째 인자로 넘겨주고,
    home 에서 받아서 보여준다.

---
+++ db.js
@@ -0,0 +1,26 @@
+export const videos = [
+    {
+        id: 111111,
+        title: 'Video 1',
+        description: 'This is something I love',
+        views: 24,
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator: {
+            ie: 121212,
+            name: 'kororo',
+            email: 'kororo@me.com',
+        },
+    },
+    {
+        id: 222222,
+        title: 'Video 2',
+        description: 'This is awsome I love',
+        views: 24,
+        videoFile: 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
+        creator: {
+            ie: 121212,
+            name: 'kororo',
+            email: 'kororo@me.com',
+        },
+    },
+];

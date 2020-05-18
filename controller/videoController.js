5cbe47e90ff9da65c3049348766b1755aefa181b
kororo11 <kororo11@naver.com>
2020-05-18 17:49:25 +0900

    Search Controller
    header.pug 에 get 방식으로 검색창을 하나 만들고 그 내용을 요청하면 controller 에서 받아서 화면에 보여준다.
    controller 에서는 객체의 구조분해를 이용해서 req.query.term 을 찾았다.

--- controller/videoController.js
+++ controller/videoController.js
@@ -1,7 +1,26 @@
 export const home = (req, res) => res.render('home', { pageTitle: 'Home' });
-export const search = (req, res) => res.render('search', { pageTitle: 'Search' });
-export const videos = (req, res) => res.render('videos', { pageTitle: 'Videos' });
-export const upload = (req, res) => res.render('upload', { pageTitle: 'Upload' });
-export const videoDetail = (req, res) => res.render('videoDetail', { pageTitle: 'Video Detil' });
-export const editVideo = (req, res) => res.render('editVideo', { pageTitle: 'Edit Video' });
-export const deleteVideo = (req, res) => res.render('deleteVideo', { pageTitle: 'Delete Video' });
+export const search = (req, res) => {
+    // ES6 이전의 방식
+    // const searchingBy = req.query.term;
+    // ES6 이후의 방식
+    // 객체 구조분해
+    const {
+        query: { term: searchingBy }, // 프로퍼티 이름 변경. term 을 searchingBy 로 변경했음.
+    } = req;
+    res.render('search', { pageTitle: 'Search', searchingBy });
+};
+export const videos = (req, res) => {
+    res.render('videos', { pageTitle: 'Videos' });
+};
+export const upload = (req, res) => {
+    res.render('upload', { pageTitle: 'Upload' });
+};
+export const videoDetail = (req, res) => {
+    res.render('videoDetail', { pageTitle: 'Video Detil' });
+};
+export const editVideo = (req, res) => {
+    res.render('editVideo', { pageTitle: 'Edit Video' });
+};
+export const deleteVideo = (req, res) => {
+    res.render('deleteVideo', { pageTitle: 'Delete Video' });
+};
